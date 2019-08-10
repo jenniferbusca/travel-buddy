4900b39d0c87c9ca35fb610c739751663b141086
jenniferbusca <jennifer.busca@gmail.com>
2019-08-09 23:01:29 -0400

    bootstrap updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
   end
 
   def googleAuth
-    @user = User.find_or_create_by(email: auth['email']) do |u|
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
       u.image = auth['info']['image']
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,5 @@ class Location < ApplicationRecord
   has_many :itinerary_activities
   has_many :activities, through: :itinerary_activities
   has_many :itineraries, through: :itinerary_activities
-
+  validates_presence_of :name
 end
